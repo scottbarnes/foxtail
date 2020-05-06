58d1855ab8b3695a0b3dfad977b7e8d60fa55685
Scott Barnes <scottreidbarnes@gmail.com>
2020-05-06 16:14:38 +0000

    changed the media directory back to be 'media'; added 'restart: always' to production.yml, and updated ALLOWED_HOSTS

--- config/settings/base.py
+++ config/settings/base.py
@@ -152,7 +152,7 @@ STATICFILES_FINDERS = [
 # MEDIA
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#media-root
-MEDIA_ROOT = str(APPS_DIR / "media1")
+MEDIA_ROOT = str(APPS_DIR / "media")
 # https://docs.djangoproject.com/en/dev/ref/settings/#media-url
 MEDIA_URL = "/media/"
 
